cdc8db0dd6aaa58faebf8e24108a83d4915e8897
bradenhurl <bradenhurl@gmail.com>
2018-11-18 16:43:05 -0500

    -Moved object detection files to separate folder

---
+++ Constants.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#define PI 3.1415926535898
+#define D2R PI/180.0
+
+//Floats per point in the pointcloud (x, y, z, reflectance)
+const int FLOATS_PER_POINT = 4;
+const float MAX_LIDAR_DIST = 120.0f;//in metres
+
+const bool SET_PED_BOXES = true;
+const float PED_BOX_WIDTH = 1.2f;
+const float PED_BOX_LENGTH = 0.5f;
+const float PED_BOX_WALKING_LEN = 1.44f;
+
+//Use the same time of day throughout the collection process
+const bool SAME_TIME_OF_DAY = true;
+//Drive in specified area or wander entire map
+const bool DRIVE_SPEC_AREA = true;
+const bool START_SPEC_AREA = true;
+
+//TODO -> stencil/depth semantic segmentation for depthmap->lidar interpolation
+
+//Some settings for testing pointcloud generation
+//This prints the 2D map of where lidar beams hit on the screen (not consistent when moving)
+const bool GENERATE_2D_POINTMAP = false;
+//Outputs secondary pointcloud with raycast points
+const bool OUTPUT_RAYCAST_POINTS = true;
+const bool OUTPUT_DM_POINTCLOUD = true;
+
+//Sends animals to client. Check client also outputs them
+const bool RETAIN_ANIMALS = true;
+
+//For testing
+const bool OUTPUT_OCCLUSION_IMAGE = true;
+const bool OUTPUT_UNUSED_PIXELS_IMAGE = true;
+
+//Can be used to get general outline of some objects with raycasting
+//WARNING: NOT ALL VEHICLES ARE HIT WITH RAYCASTING
+const bool OBTAIN_RAY_POINTS_HIT = false;
+//Overlays adjusted points on top of pointcloud (using raycasting + depth map)
+const bool OUTPUT_ADJUSTED_POINTS = false;
+
+//2d points will be shrunk with stencil cull
+const bool CORRECT_2D_POINTS_BEHIND_CAMERA = false;
+
+const bool LIDAR_GAUSSIAN_NOISE = true;
+const double DEPTH_NOISE_STDDEV = 0.006;//3 standard deviations is approximately 2cm
+const double DEPTH_NOISE_MEAN = 0.0;
+
+//Extends bboxes/segmentation for bike type vehicles with rider (ped) information
+const bool PROCESS_PEDS_ON_BIKES = true;
---
+++ ObjectDetection.h
@@ -0,0 +1,178 @@
+#pragma once
+
+#include <stdlib.h>
+#include <ctime>
+
+#include "LiDAR.h"
+#include "Functions.h"
+#include "CamParams.h"
+#include "FrameObjectInfo.h"
+
+//#define DEBUG 1
+
+class ObjectDetection {
+public:
+    ObjectDetection();
+    ~ObjectDetection();
+private:
+    FrameObjectInfo m_curFrame;
+    bool m_initialized = false;
+    bool m_eve = false;
+    Vector3 m_eveForwardVector;
+
+    Vehicle vehicle = NULL;
+    Player player = NULL;
+    Ped ped = NULL;
+    Cam camera = NULL;
+    Vector3 dir;
+
+    float x, y, z;
+    float startHeading;
+    int hour, minute;
+    const char* _weather;
+    const char* _vehicle;
+
+    bool pointclouds;
+
+    std::string baseFolder;
+
+    float currentThrottle = 0.0;
+    float currentBrake = 0.0;
+    float currentSteering = 0.0;
+
+    Vector3 currentPos;
+    Vector3 vehicleForwardVector;
+    Vector3 vehicleUpVector;
+    Vector3 vehicleRightVector;
+
+    Vector3 m_camForwardVector;
+    Vector3 m_camRightVector;
+    Vector3 m_camUpVector;
+
+    bool running = false;
+
+    int m_startArea = 1; //Downtown (see s_locationBounds)
+    std::vector<std::vector<char>> m_polyGrid;
+
+    //LiDAR variables
+    LiDAR lidar;
+    bool lidar_initialized = false;
+    int pointCloudSize = 0;
+    std::unordered_map<int, HitLidarEntity*> entitiesHit;
+    int lidar_param = 7;
+
+    //Depth Map variables
+    float* m_pDepth = NULL;
+    uint8_t* m_pStencil = NULL;
+    float* m_pDMPointClouds = NULL;
+    uint16_t* m_pDMImage = NULL;
+    uint8_t* m_pStencilImage = NULL;
+    unsigned char* color_buf = NULL;
+    uint8_t* m_pStencilSeg = NULL;
+    int m_stencilSegLength = 0;
+    uint8_t* m_pOcclusionImage = NULL;
+    uint8_t* m_pUnusedStencilImage = NULL;
+
+    bool vehicles_created = false;
+    std::vector<VehicleToCreate> vehiclesToCreate;
+    std::vector<PedToCreate> pedsToCreate;
+
+    //For tracking: first frame in a series that an entity appears
+    std::unordered_map<int, int> trackFirstFrame;
+    //For calculating real speed
+    Vector3 m_trackLastPos;
+    float m_trackRealSpeed;
+    float m_trackLastRealSpeed;
+    float m_trackDist;
+    float m_trackDistVar;
+    float m_trackDistErrorTotal;
+    float m_trackDistErrorTotalVar;
+    float m_trackDistErrorTotalCount;
+    int m_trackLastIndex = -1;
+    int m_trackLastSeqIndex = 0;
+    std::string m_timeTrackFile;
+    std::string m_usedPixelFile;
+
+    //Camera intrinsic parameters
+    float intrinsics[3];
+
+    bool m_vLookupInit = false;
+    std::unordered_map<std::string, std::string> m_vLookup; //Vehicle lookup
+
+    std::unordered_map<Vehicle, std::vector<Ped>> m_pedsInVehicles;
+
+public:
+    void initCollection(UINT camWidth, UINT camHeight, bool exportEVE = true);
+    void setCamParams(float* forwardVec = NULL, float* rightVec = NULL, float* upVec = NULL);
+
+    //Depth buffer fn/var needs to be accessed by server
+    void setDepthBuffer(bool prevDepth = false);
+    bool m_prevDepth = false;
+
+    FrameObjectInfo generateMessage(float* pDepth, uint8_t* pStencil);
+    void exportDetections();
+    void exportImage(BYTE* data);
+    void increaseIndex();
+
+    int instance_index = 0;
+    int series_index = 0;
+    std::string series_string = "0000";
+    std::string instance_string;
+    int baseTrackingIndex = instance_index;
+
+    //Tracking variables
+    bool collectTracking;
+    //# of instances in one series
+    const int trSeriesLength = 500;
+    //# of seconds between series
+    const int trSeriesGapTime = 30;
+    //Used for keeing track of when to add the gap
+    bool trSeriesGap = false;
+
+private:
+    void setVehiclesList();
+    void setPedsList();
+    void setTrafficSignsList();
+    void setDirection();
+    void setReward();
+    void setThrottle();
+    void setBrake();
+    void setSteering();
+    void setSpeed();
+    void setYawRate();
+    void setDrivingMode();
+    void setTime();
+    void setupLiDAR();
+    void collectLiDAR();
+    void setIndex();
+    void calcCameraIntrinsics();
+    void setFocalLength();
+    bool getEntityVector(ObjEntity &entity, int entityID, Hash model, int classid, std::string type, std::string modelString);
+    void setPosition();
+    float observationAngle(Vector3 position);
+    void drawVectorFromPosition(Vector3 vector, int blue, int green);
+    void setDepthParams();
+    Vector3 depthToCamCoords(float depth, float screenX, float screenY);
+    std::string getStandardFilename(std::string subDir, std::string extension);
+    void outputRealSpeed();
+    void setStencilBuffer();
+
+    BBox2D BBox2DFrom3DObject(Vector3 position, Vector3 dim, Vector3 forwardVector, Vector3 rightVector, Vector3 upVector, bool &success, float &truncation);
+    BBox2D processBBox2D(BBox2D bbox, uint8_t stencilType, Vector3 position, Vector3 dim, Vector3 forwardVector, Vector3 rightVector, Vector3 upVector,
+        Vector3 xVector, Vector3 yVector, Vector3 zVector, int entityID, int &pointsHit2D, float &occlusion);
+    bool in3DBox(Vector3 point, Vector3 objPos, Vector3 dim, Vector3 yVector, Vector3 xVector, Vector3 zVector);
+    bool checkDirection(Vector3 unit, Vector3 point, Vector3 min, Vector3 max);
+    void printSegImage();
+
+    bool hasLOSToEntity(Entity entityID, Vector3 position, Vector3 dim, Vector3 forwardVector, Vector3 rightVector, Vector3 upVector);
+
+    void initVehicleLookup();
+    bool isPointOccluding(Vector3 worldPos, Vector3 position);
+    void outputOcclusion();
+    void outputUnusedStencilPixels();
+
+    //Export functions
+    void exportEntity(ObjEntity e, std::ostringstream& oss);
+    void exportEntities(EntityMap entMap, std::ostringstream& oss);
+    void exportCalib();
+};
