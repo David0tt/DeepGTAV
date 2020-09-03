ab65f72b0837e13dc50533322664ebf1ec7335d4
Eisbaer8 <eisbaer8@live.de>
2020-09-03 04:11:18 +0200

    Working StartRecording and StopRecording messages

--- Constants.h
+++ Constants.h
@@ -108,5 +108,3 @@ const bool PROCESS_OVERLAPPING_POINTS = false;
 //Outputs all vehicles within range in augmented labels
 const bool AUGMENT_ALL_VEHICLES_IN_RANGE = true;
 
-// This disables the Data Capture. This is mainly used for debugging purposes (esp. for the wayfinding)
-const bool NO_CAPTURE_FOR_DEBUG = true;
