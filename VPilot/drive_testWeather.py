790b7627cff3aec18865460f9c34256956440cad
Eisbaer8 <eisbaer8@live.de>
2020-11-26 00:17:42 +0100

    Restructuring for easy building developement environment for DeepGTAV

--- VPilot/drive_testWeather.py
+++
@@ -1,4 +0,0 @@
-            weathers = ["CLEAR", "EXTRASUNNY", "CLOUDS", "OVERCAST", "RAIN", "CLEARING", "THUNDER", "SMOG", "FOGGY", "XMAS", "SNOWLIGHT", "BLIZZARD", "NEUTRAL", "SNOW"]
-
-            client.sendMessage(SetWeather(weathers[count % 14]))
-            print(weathers[count%14])
